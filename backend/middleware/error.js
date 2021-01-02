00491b464269a4c179b30dcd167b081e8459ff41
Sammy-Nyakabau <sammy@sammynyakabau.com>
2021-01-02 18:15:57 +0530

    ✏️ Fix Typo: on error middleware

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -5,7 +5,7 @@ const debug = require("debug")("app:error");
 module.exports = function (err, _req, res, _next) {
   logger.error(err.message, err);
   debug(err.message[0]);
-  console.log(chalk.red(err.message[1]));
+  console.log(chalk.red(err.message[0]));
 
   res.status(500).send("Something failed.");
 };
