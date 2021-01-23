c2f2bd65505e04de18cba40c26e1e83d312fe8d4
Sammy-Nyakabau <sammy@sammynyakabau.com>
2021-01-23 23:40:19 +0530

    🎨 Improved formatting: change folder structure

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import ScrollToTop from "./components/common/ScrollToTop";
+import ScrollToTop from "../shared/ScrollToTop";
 
 import "./App.css";
 
